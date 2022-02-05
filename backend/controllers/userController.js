c86cbe1f109552209624aa1ccac7ddcc69b41f50
kyle <kyle@melissadata.com>
2022-02-04 17:12:14 -0800

    Added user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+
 // @desc Register a user
 // @route /api/users
 // @access Public
@@ -8,7 +11,36 @@ const registerUser = asyncHandler(async (req, res) => {
   // Validation
   if(!name || !email || !password) {
     res.status(400);
-    throw new Error('Please include all fields');
+    throw new error('Please include all fields');
+  }
+
+  // Find if user exists
+  const userExsits = await User.findOne({email});
+  if (userExsits) {
+    res.status(400);
+    throw new error('User already exsits');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400);
+    throw new error('Invalid user data')
   }
 
   res.send('Register Route');
