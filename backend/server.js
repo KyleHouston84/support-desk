c4d0744a1cdd2672c9971c09aba621d2567680ea
kyle <kyle@melissadata.com>
2022-02-09 08:28:23 -0800

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -13,12 +14,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the support tick app!' });
-})
-
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve front end
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+} else {
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the support tick app!' });
+  });
+}
 
 app.use(errorHandler);
 
