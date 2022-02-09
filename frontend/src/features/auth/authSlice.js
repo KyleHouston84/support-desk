eeeeda1c4d45f5b69f96e6730a58db8ea6547388
kyle <kyle@melissadata.com>
2022-02-09 00:22:06 -0800

    Added login functionality

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -26,7 +26,14 @@ export const register = createAsyncThunk('auth/register', async (user, thunkAPI)
 
 // Login User
 export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
-  console.log(user, thunkAPI);
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+    console.error(error.response.data.stack);
+
+    return thunkAPI.rejectWithValue(message);
+  }
 });
 
 // Logout user
@@ -56,6 +63,20 @@ export const authSlice = createSlice({
         state.user = action.payload
       })
       .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaSignInAlt } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
-import { login } from '../features/auth/authSlice';
+import { login, reset } from '../features/auth/authSlice';
 
 function Login() {
   const [ formData, setFormData ] = useState({
@@ -13,8 +14,17 @@ function Login() {
   const { email, password } = formData;
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { user, isLoading, isSuccess, message } = useSelector(state => state.auth);
+  const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (isError) toast(message);
+
+    if (isSuccess) navigate('/');
+
+    dispatch(reset());
+  }, [isError, isSuccess, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => (
