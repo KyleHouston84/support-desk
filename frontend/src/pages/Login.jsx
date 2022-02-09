506036b4a0ba0765f891e00148e6c718a29d3b1b
kyle <kyle@melissadata.com>
2022-02-08 22:38:05 -0800

    Added login/register forms and home pages buttons.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
+import { FaSignInAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const {email, password} = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => (
+      {
+        ...prevState,
+        [e.target.id]: e.target.value
+      }
+    ));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login to get support</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input type="email" className="form-control" id='email' value={email} onChange={onChange} required placeholder="enter your email"/>
+          </div>
+          <div className="form-group">
+            <input type="password" className="form-control" id='password' value={password} onChange={onChange} required placeholder="enter password"/>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
